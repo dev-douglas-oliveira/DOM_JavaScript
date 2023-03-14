c182dbf82385ba9c8c584b07e056b9c6cde09df9
Douglas Oliveira <dougmoroli@gmail.com>
2023-03-14 17:44:05 -0300

    criando lista de tarefas

--- 1823_DOM-projeto_incial/projeto_inicial/assets/js/listaDeTarefas.js
+++ 1823_DOM-projeto_incial/projeto_inicial/assets/js/listaDeTarefas.js
@@ -1,9 +1,18 @@
 const criarTarefa = function (evento) {
     evento.preventDefault();
-
+    const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
-    console.log(valor);
+    //console.log(valor);
+    //const tarefa = document.querySelector("[data-list]");
+
+    const tarefa = document.createElement("li");
+
+    const conteudo = `<p class="content">${valor}</p>`;
+    tarefa.classList.add("task"); //para criar uma classe "task" no html
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa); //anexa um elemento dentro de outro
+
     input.value = "";
 };
 const novaTarefa = document.querySelector("[data-form-button]");
