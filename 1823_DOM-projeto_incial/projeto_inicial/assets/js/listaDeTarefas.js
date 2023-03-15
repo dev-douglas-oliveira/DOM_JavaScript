28cc521427fa7a15478421f56d01aaefa18773cf
Douglas Oliveira <dougmoroli@gmail.com>
2023-03-15 15:31:14 -0300

    adicionando o botão de concluir

--- 1823_DOM-projeto_incial/projeto_inicial/assets/js/listaDeTarefas.js
+++ 1823_DOM-projeto_incial/projeto_inicial/assets/js/listaDeTarefas.js
@@ -1,19 +1,37 @@
-const criarTarefa = function (evento) {
-    evento.preventDefault();
-    const lista = document.querySelector("[data-list]");
-    const input = document.querySelector("[data-form-input]");
-    const valor = input.value;
-    //console.log(valor);
-    //const tarefa = document.querySelector("[data-list]");
+(function () {
+    const criarTarefa = function (evento) {
+        evento.preventDefault();
 
-    const tarefa = document.createElement("li");
+        const lista = document.querySelector("[data-list]");
+        const input = document.querySelector("[data-form-input]");
+        const valor = input.value;
 
-    const conteudo = `<p class="content">${valor}</p>`;
-    tarefa.classList.add("task"); //para criar uma classe "task" no html
-    tarefa.innerHTML = conteudo;
-    lista.appendChild(tarefa); //anexa um elemento dentro de outro
+        const tarefa = document.createElement("li");
+        tarefa.classList.add("task"); //para criar uma classe "task" no html
+        const conteudo = `<p class="content">${valor}</p>`;
 
-    input.value = "";
-};
-const novaTarefa = document.querySelector("[data-form-button]");
-novaTarefa.addEventListener("click", criarTarefa);
+        tarefa.innerHTML = conteudo;
+
+        tarefa.appendChild(BotaoConclui());
+        lista.appendChild(tarefa); //anexa um elemento dentro de outro
+        input.value = "";
+    };
+    const novaTarefa = document.querySelector("[data-form-button]");
+    novaTarefa.addEventListener("click", criarTarefa);
+
+    const BotaoConclui = function () {
+        const botaoConclui = document.createElement("button");
+        botaoConclui.classList.add("check-button");
+        botaoConclui.innerText = "concluir";
+        botaoConclui.addEventListener("click", concluirTarefa);
+        return botaoConclui;
+    };
+
+    const concluirTarefa = function (evento) {
+        const botaoConclui = evento.target;
+
+        const tarefaCompleta = botaoConclui.parentElement;
+
+        tarefaCompleta.classList.toggle("done");
+    };
+})();
